c4399cd89d72b149828c1b3fe0860d4e872451bc
Maryna-Hemb <marinagembarskaa@gmail.com>
2022-12-01 17:10:28 +0200

    add task 1-3

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+const categoriesItemEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItemEl.length}`);
+
+categoriesItemEl.forEach(item => {
+  const categoryListNameEl = item.querySelector('h2');
+  console.log(`Category: ${categoryListNameEl.textContent}`);
+  const categoryInnerList = item.querySelectorAll('li');
+  console.log(`Elements: ${categoryInnerList.length}`);
+});
+
+// ВАРІАНТИ РОЗВ'ЯЗКУ альтернативні
+
+// A) for... of
+// for (const categoryListDataEl of categoriesItemEl) {
+//   const categoryListNameEl = categoryListDataEl.querySelector('h2');
+//   console.log(`Category: ${categoryListNameEl.textContent}`);
+//   const categoryInnerList = categoryListDataEl.querySelectorAll('li');
+//   console.log(`Elements: ${categoryInnerList.length}`);
+// }
---
+++ js/task-02.js
@@ -0,0 +1,53 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsItemsEl = document.querySelector('#ingredients');
+
+const makeMarkupIngredientsEl = ingredients => {
+  return ingredients.map(ingredient => {
+    const listItemEl = document.createElement('li');
+    listItemEl.textContent = ingredient;
+    listItemEl.classList.add('item');
+    return listItemEl;
+  });
+};
+
+const fullListItemsEl = makeMarkupIngredientsEl(ingredients);
+ingredientsItemsEl.append(...fullListItemsEl);
+console.log(ingredientsItemsEl);
+
+// ВАРІАНТИ РОЗВ'ЯЗКУ альтернативні
+// Перебирання масиву
+
+// A) forEach + .push()
+
+// const fullListItemsEl = [];
+// ingredients.forEach(ingredient => {
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = ingredient;
+//   listItemEl.classList.add('item');
+//   fullListItemsEl.push(listItemEl);
+// });
+// ingredientsItemsEl.append(...fullListItemsEl);
+
+// Б) .map()
+
+// const fullListItemsEl = ingredients.map(ingredient => {
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = ingredient;
+//   listItemEl.classList.add('item');
+//   return listItemEl;
+// });
+// ingredientsItemsEl.append(...fullListItemsEl);
+
+// В) функція для одного елемента як параметра для .map()
+
+// const makeMarkupIngredientsEl = ingredient => {
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = ingredient;
+//   listItemEl.classList.add('item');
+//   return listItemEl;
+// };
+
+// const fullListItemsEl = ingredients.map(makeMarkupIngredientsEl);
+// ingredientsItemsEl.append(...fullListItemsEl);
+// console.log(ingredientsItemsEl);
